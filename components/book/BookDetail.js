98e25e1d2469489dbf3fa61cfaecd336bfc474c6
Denis Costello <G00396309@atu.ie>
2023-12-13 10:32:33 +0000

    Changed details

--- components/book/BookDetail.js
+++ components/book/BookDetail.js
@@ -1,21 +1,27 @@
-import classes from './BookDetail.module.css'
+import classes from './BookDetail.module.css';
 
 function BookDetail(props) {
-    return (
-        <section className={classes.detail}>
-            <div className={classes.bookDetails}>
-                <div className={classes.imageDiv}>
-                    <img src={props.poster} alt={props.title} />
-                </div>
-                <div className={classes.textDiv}>
-                    <h1>{props.title}</h1>
-                    <h2>{props.author}</h2>
-                    <h2>{props.releaseDate}</h2>
-                    <a href={props.reviewLink}>Review on Youtube!</a>
-                </div>
+  return (
+    <section className={classes.detail}>
+      <div className={classes.bookDetails}>
+      <div style={{ fontSize: '3rem', fontWeight: 'bold', color: '#800000' }}>
+            {props.title}
+        </div>
+        <div className={classes.imageDiv}>
+          <img src={props.poster} alt={props.title} />
+        </div>
+        <div className={classes.textDiv}>
+          <h2>{props.author}</h2>
+          <h2 style={{ marginBottom: '1em' }}>{props.releaseDate}</h2>
+          <a href={props.reviewLink} className={classes.reviewLink}>
+            <div style={{ border: '2px solid #ffcccc', backgroundColor: '#ffe6e6', padding: '1em', borderRadius: '8px', display: 'inline-block', paddingTop: '0.5em' }}>
+                Review on Youtube!
             </div>
-        </section>
-    )
+            </a>
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default BookDetail
+export default BookDetail;
